083be2e0107ff6b23955f60d99970737726495f5
Colin Bruin <colinjb23@gmail.com>
2018-05-03 10:54:58 -0400

    adding a new port option

--- app.js
+++ app.js
@@ -67,4 +67,4 @@ app.get('/guests', (req, res) => {
     });  
 })
 
-app.listen(process.env.PORT || 5000), () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'))
